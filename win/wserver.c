e4420a5d692bb5b2236901ff81f6bcda735af6b7
Emanuele Giona <emanuelegiona@users.noreply.github.com>
2017-10-09 13:58:16 +0200

    License update

--- win/wserver.c
+++ win/wserver.c
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2017 Emanuele Giona
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include "wserver.h"
 
 int XOR(int a, int b) {
@@ -253,7 +277,7 @@ int fileXOR(char srcfile[], char dstfile[], long long dim, int seed) {
 				return 500;
 			}
 
-			//arrivo correttamente all'inizio del mapping, bypassando la granularit‡
+			//arrivo correttamente all'inizio del mapping, bypassando la granularit√†
 			srcmap += offset;
 
 			char *dstmap = (char *)MapViewOfFile(handle_dstmap, FILE_MAP_ALL_ACCESS, fileMapStart.HighPart, fileMapStart.LowPart, mapViewSize.QuadPart);
@@ -272,7 +296,7 @@ int fileXOR(char srcfile[], char dstfile[], long long dim, int seed) {
 				return 500;
 			}
 
-			//arrivo correttamente all'inizio del mapping, bypassando la granularit‡
+			//arrivo correttamente all'inizio del mapping, bypassando la granularit√†
 			dstmap += offset;
 
 			//1 thread OpenMP ogni 256KB
