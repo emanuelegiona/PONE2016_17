c08260a108b19d4a4ba6fdd84afee8a57c1e89ac
Emanuele Giona <emanuelegiona@users.noreply.github.com>
2017-10-09 13:58:33 +0200

    License update

--- win/wclient.c
+++ win/wclient.c
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2017 Emanuele Giona
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include "wclient.h"
 
 int initDir() {
@@ -156,7 +180,7 @@ DWORD WINAPI wthread_LSTF(LPVOID arg) {
 		return 1;
 	}
 
-	//ricezione finché server non chiude connessione
+	//ricezione finchÃ© server non chiude connessione
 	bool first = true;
 	while (true) {
 		ZeroMemory(message,BUFSIZE);
@@ -276,7 +300,7 @@ DWORD WINAPI wthread_LSTR(LPVOID arg) {
 		return 1;
 	}
 
-	//ricezione finché server non chiude connessione
+	//ricezione finchÃ© server non chiude connessione
 	bool first = true;
 	while (true) {
 		ZeroMemory(message,BUFSIZE);
@@ -400,7 +424,7 @@ DWORD WINAPI wthread_ENCR(LPVOID arg) {
 		return 1;
 	}
 
-	//ricezione finché server non chiude connessione
+	//ricezione finchÃ© server non chiude connessione
 	bool first = true;
 	while (true) {
 		res = recv(sock, message, BUFSIZE, 0);
@@ -524,7 +548,7 @@ DWORD WINAPI wthread_DECR(LPVOID arg) {
 		return 1;
 	}
 
-	//ricezione finché server non chiude connessione
+	//ricezione finchÃ© server non chiude connessione
 	bool first = true;
 	while (true) {
 		res = recv(sock, message, BUFSIZE, 0);
